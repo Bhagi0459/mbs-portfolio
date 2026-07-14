a4923ed140d7fe56abaa0b86c83db68679023407
Bhagya Sankar Maddela <m.bhagyasankar@gmail.com>
2026-07-14 14:43:31 +0530

    Added intro

---
+++ src/app/shared/ambient-background/ambient-background.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ambient-background',
+  templateUrl: './ambient-background.html',
+  styleUrl: './ambient-background.scss',
+})
+export class AmbientBackground {}
